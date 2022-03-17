100a4a9f31a45763f4b09db66554c25da9be8e25
Keneth Riera <kenethrr@outlook.com>
2022-03-16 23:06:09 -0500

    add customer struct

---
+++ domain/customer.go
@@ -0,0 +1,29 @@
+package domain
+
+type Customer struct {
+	Id          string
+	Name        string
+	City        string
+	ZipCode     string
+	DateOfBirth string
+	Status      string
+}
+
+type CustomerRepository interface {
+	FindAll() ([]Customer, error)
+}
+
+type CustomerRepositoryStub struct {
+	customers []Customer
+}
+
+func (s CustomerRepositoryStub) FindAll() ([]Customer, error) {
+	return s.customers, nil
+}
+
+func NewCustomerRepositoryStub() CustomerRepositoryStub {
+	customers := []Customer{
+		{Id: "1001", Name: "Keneth", City: "Guayaquil", ZipCode: "12345", DateOfBirth: "2000-02-12", Status: "1"},
+	}
+	return CustomerRepositoryStub{customers}
+}
