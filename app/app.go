253171386ee94f5c146a2631297b888d347d789a
Keneth Riera <kenethrr@outlook.com>
2022-05-02 23:52:45 -0500

    change logger in main

--- app/app.go
+++ app/app.go
@@ -1,11 +1,11 @@
 package app
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/kenethrrizzo/banking/domain"
+	"github.com/kenethrrizzo/banking/logger"
 	"github.com/kenethrrizzo/banking/service"
 )
 
@@ -18,5 +18,5 @@ func Start() {
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
-	log.Fatalln(http.ListenAndServe("localhost:8080", router))
+	logger.Error(http.ListenAndServe("localhost:8080", router).Error())
 }
